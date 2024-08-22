52c82184b7f391808401d3f1ee282438a4477a14
aliciayerinkim <aliciayerinkim@gmail.com>
2024-08-21 22:34:47 -0700

    selected contact view

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,16 @@ import { useState } from 'react'
 import ContactList from './components/ContactList.jsx'
 
 function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
 
   return (
     <>
-      <ContactList />
+      {selectedContactId ? (
+        <div>Selected Contact View</div>
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId}  />
+      )}
+      
     </>
   )
 }
